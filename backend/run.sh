6f62ed2415d9b761cf7ca19581b813ba95eb203a
bassamadnan <bassamadnan2012@gmail.com>
2024-05-30 22:55:07 +0530

    [fix] move to hypercorn for windows compatibility

--- backend/run.sh
+++ backend/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $1 == "--test" ]]; then
-    uvicorn main:app --host 0.0.0.0 --port 8000 --log-level debug --reload
+    hypercorn main:app --bind 0.0.0.0:8000 --log-level debug --reload
 else
     # print the value of the WORKERS environment variable
     echo "WORKERS: ${WORKERS:-1}"
-    gunicorn main:app --workers=${WORKERS:-1} -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --preload
+    hypercorn main:app --workers ${WORKERS:-1} --bind 0.0.0.0:8000
 fi
