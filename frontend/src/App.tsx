85bfa8dcd71f3da0d0b2774e2815f8531c306960
animesh <animeshchaudhri3@gmail.com>
2024-05-31 02:05:44 +0530

    feat: Initialize Vite React app with Tailwind CSS, integrate Tauri v1, and add dev scripts for Linux and Windows

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello rust</h1>
+    </div>
+  );
+}
+
+export default App;
