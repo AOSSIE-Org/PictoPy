6c48a8e6f416ffa2ea0bbb1910936c68f2cdc1ea
Shalin Agrawal <shalinagrawal2@gmail.com>
2024-11-29 19:35:39 +0530

    added test for ner

--- backend/app/image_captioning/test.py
+++ backend/app/image_captioning/test.py
@@ -1,27 +1,53 @@
-import os
-import sqlite3
-import numpy as np
-from PIL import Image
-from app.image_captioning.caption import caption1
-from app.config.settings import IMAGES_DATABASE_PATH
+# to perform ner on the query and the caption to find similar keys in ner-query dictionary and the ner-caption dictionary
 
 
-def captioning_images():
-    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT path FROM image_id_mapping;")
-    paths = [row[0] for row in cursor.fetchall()]  
-    
-    conn.close()
-    captions = []
-    for i in paths:
-        img = Image.open(os.path.abspath(i))  
-        img = np.array(img) 
-        caption = caption1(img)
-        print(f"{os.path.abspath(i)}:{caption}")
-        captions.append(caption)
-    print(captions)
-    print(paths)
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-captioning_images()    
+model_name = "dslim/bert-base-NER"  
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
+
+
+ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer, grouped_entities=True)
+query = "Elon Musk was seen in Austin, Texas, during a SpaceX event."
+caption = "Show me pictures of Elon Musk in Texas."
+query_entities = ner_pipeline(query)
+caption_entities = ner_pipeline(caption)
+
+
+query_extracted = {entity['word']: entity['entity_group'] for entity in query_entities}
+caption_extracted = {entity['word']: entity['entity_group'] for entity in caption_entities}
+
+print("Query Entities:", query_extracted)
+print("Caption Entities:", caption_extracted)
+
+def merge_entities(query_extracted, caption_extracted):
+    merged = {}
+    skip = False
+    keys = list(caption_extracted.keys())
+
+    for i, key in enumerate(keys):
+        if skip:
+            skip = False
+            continue
+
+        if i < len(keys) - 1:
+
+            combined_entity = " ".join([key, keys[i + 1]])
+
+            if combined_entity in query_extracted and combined_entity not in merged:
+                merged[combined_entity] = query_extracted[combined_entity]
+                skip = True  
+            else:
+                merged[key] = caption_extracted[key]
+        else:
+            merged[key] = caption_extracted[key]
+
+    return merged        
+
+merged_caption = merge_entities(query_extracted, caption_extracted)
+merged_query = merge_entities(caption_extracted, query_extracted)
+common_entities = set(merged_query.keys()) & set(merged_caption.keys())
+
+print("Common Entities:", common_entities)
+print("length:", len(common_entities))
