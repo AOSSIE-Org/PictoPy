141e0aa7ea79e320f898ef41f4281803ab39dcb5
Jibesh-dev-89 <jibeshr60@gmail.com>
2024-12-16 16:03:18 +0530

    Added images folder

--- frontend/src/layout/main.tsx
+++ frontend/src/layout/main.tsx
@@ -4,6 +4,7 @@ import Sidebar from '@/components/Navigation/Sidebar/Sidebar';
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex w-full flex-col bg-gray-900">
+      <Navbar />
       <div className="sidebar flex" style={{ height: 'calc(100vh - 64px)' }}>
         <Sidebar />
         <div className="flex flex-1 overflow-x-auto p-4 text-white">
