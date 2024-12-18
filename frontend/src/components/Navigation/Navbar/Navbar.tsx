7e369493700be04edc4409777b3ab173baea3369
Dushyantbha012 <89176588+Dushyantbha012@users.noreply.github.com>
2024-12-18 13:58:19 +0530

    Update Navbar fixed closing tags issue

--- frontend/src/components/Navigation/Navbar/Navbar.tsx
+++ frontend/src/components/Navigation/Navbar/Navbar.tsx
@@ -2,17 +2,21 @@ export function Navbar(props: { title?: string }) {
   return (
     <>
       <header className="flex w-full flex-row items-center justify-center align-middle">
-      <div className="flex h-16 items-center justify-between bg-[#333333] px-16 w-[50%] mt-3 rounded-3xl ">
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-2">
-            <img src="/tauri.svg" height={"20px"} width={"20px"} alt="" />
-            <span className="font-sans text-lg font-bold text-gray-50">
-              Pictopy
+        <div className="mt-3 flex h-16 w-[50%] items-center justify-between rounded-3xl bg-[#333333] px-16 mb-4">
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
+              <img src="/tauri.svg" height={'20px'} width={'20px'} alt="" />
+              <span className="font-sans text-lg font-bold text-gray-50">
+                Pictopy
+              </span>
+            </div>
+          </div>
           <div className="flex items-center gap-4">
             <span className="font-sans text-lg font-medium text-gray-50">
               Welcome {props.title || 'User'}
             </span>
           </div>
+        </div>
       </header>
     </>
   );
