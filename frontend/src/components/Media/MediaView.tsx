c153cba31bc0700b661e285a2eb755bfb3ece9d9
Dushyant Bhardwaj <dushyantbhardwaj@AirStalin.local>
2024-12-18 14:19:56 +0530

    fixed bug not showing image in center and styled buttons

--- frontend/src/components/Media/MediaView.tsx
+++ frontend/src/components/Media/MediaView.tsx
@@ -116,7 +116,7 @@ const handelZoomIn = ()=>{
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
             onMouseLeave={handleMouseUp}
-            className="relative h-full w-full overflow-hidden"
+            className="relative h-full w-full overflow-hidden flex items-center justify-center align-middle" 
           >
             <img
               src={allMedia[globalIndex]}
@@ -131,19 +131,19 @@ const handelZoomIn = ()=>{
             <div className="absolute bottom-4 right-4 flex gap-2">
               <button
                 onClick={handelZoomOut}
-                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black"
+                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black transition duration-200 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)]"
               >
                 -
               </button>
               <button
                 onClick={resetZoom}
-                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black"
+                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black transition duration-200 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)]"
               >
                 Reset
               </button>
               <button
                 onClick={handelZoomIn}
-                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black"
+                className="rounded-md border border-black bg-white px-4 py-2 text-sm text-black transition duration-200 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)]"
               >
                 +
               </button>
