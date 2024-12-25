c4e6abf74e80d8129c86d828843839a73ea7b1d1
Abhishek Singh <as8998013@gmail.com>
2024-12-26 00:48:03 +0530

    managed conflicts

--- frontend/src/pages/Setupscreen/Setup.tsx
+++ frontend/src/pages/Setupscreen/Setup.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInitialPageController } from '@/controllers/SetupController';
 import { SetupScreen } from '@/features/Setup/Setup';
 import { LoadingScreen } from '@/components/LoadingScreen/LoadingScreen';
 
 export const InitialPage: React.FC = () => {
-  const { loading, handleFolderPathChange } = useInitialPageController();
+  const {  handleFolderPathChange } = useInitialPageController();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+  }, []);
   
   if (loading) {
     return <LoadingScreen />;
   }
+  
   return <SetupScreen onFolderPathChange={handleFolderPathChange} />;
 };
