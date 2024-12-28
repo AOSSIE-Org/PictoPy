a747ce24565130e35b429c90940e009eb4956d52
Dushyant Bhardwaj <dushyantbhardwaj@AirStalin.local>
2024-12-28 14:36:45 +0530

    updated tsconfig

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
   darkMode: ["class"],
   content: [
@@ -73,5 +74,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
