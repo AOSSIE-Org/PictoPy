e37bf4ef8df093a465b4d2aa7338633e148d4577
anmolgupta2015 <anmolgupta9872148833@gmail.com>
2025-01-24 22:33:03 +0530

    Implement file locking in CacheRepository to resolve concurrency issues

--- frontend/src-tauri/src/repositories/cache_repository.rs
+++ frontend/src-tauri/src/repositories/cache_repository.rs
@@ -1,17 +1,17 @@
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 use std::path::PathBuf;
+use fs2::FileExt;
 
 pub struct CacheRepository;
 
 impl CacheRepository {
     pub fn read_cache(cache_file_path: &str) -> Option<Vec<PathBuf>> {
-        File::open(cache_file_path).ok().map(|file| {
-            let reader = BufReader::new(file);
-            reader.lines()
-                .filter_map(|line| line.ok().map(PathBuf::from))
-                .collect()
-        })
+        let file = File::open(cache_file_path).ok()?;
+        let reader = BufReader::new(file);
+        reader.lines()
+            .filter_map(|line| line.ok().map(PathBuf::from))
+            .collect()
     }
 
     pub fn write_cache(cache_file_path: &str, paths: &[PathBuf]) -> std::io::Result<()> {
@@ -20,12 +20,16 @@ impl CacheRepository {
             .create(true)
             .truncate(true)
             .open(cache_file_path)?;
+        
+        file.lock_exclusive()?;
 
         for path in paths {
             if let Some(path_str) = path.to_str() {
                 writeln!(file, "{}", path_str)?;
             }
         }
+
+        file.unlock()?;
         Ok(())
     }
 
