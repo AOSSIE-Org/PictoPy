0b43f0b689c8a38944b0b954373d52598d5b97aa
Rahul Harpal <rahulharpal91@gmail.com>
2025-01-28 18:54:12 +0530

    Add sample private key

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>,
 );
 
-
 if (isProd()) {
   onCloseListener();
   console.log('Starting PictoPy Server');
