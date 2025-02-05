a1378d54f23580298b18bd67248c27362fdc01e1
rishab <kumarjharishab@gmail.com>
2025-02-05 20:00:12 +0530

    added workflow

--- frontend/src/utils/isProd.ts
+++ frontend/src/utils/isProd.ts
@@ -1,3 +1,3 @@
 // Usage: import { isProd } from '@/utils/isProd';
 // Utility function to check if the environment is in production mode
-export const isProd = () =>import.meta.env.PROD;
+export const isProd = () =>process.env.PROD;
