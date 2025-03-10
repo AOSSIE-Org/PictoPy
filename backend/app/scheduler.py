75b79acc58e779fd668b1bf66e85fdaad84255ef
Jibesh Roy <jibeshr60@gmail.com>
2025-03-10 13:27:36 +0530

    solving lint issue via pre-commit run --config ../.pre-commit-config.yaml --all-files || exit 1

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -103,5 +103,5 @@ def run_async_task():
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(run_async_task, "interval", seconds=1)
+    scheduler.add_job(run_async_task, "interval", minutes=15)
     scheduler.start()
