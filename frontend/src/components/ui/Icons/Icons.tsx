f0118fc2c253f497e230e2f6ff4c26a9266a3617
Arham <arhamreza01@gmail.com>
2025-03-16 03:22:20 +0530

    Update Icons.tsx

--- frontend/src/components/ui/Icons/Icons.tsx
+++ frontend/src/components/ui/Icons/Icons.tsx
@@ -11,7 +11,7 @@ export function FileIcon(props: any) {
     </svg>
   );
 }
-export function Crops(props: any) {
+export function Crops() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
