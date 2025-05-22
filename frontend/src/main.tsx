a0c4ec2e63ab6103199ddc09928fe7bd3d00654f
Rahul Harpal <rahulharpal91@gmail.com>
2025-05-22 17:29:19 +0530

    Refactor updater logic and remove Windows build configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,40 @@ import { isProd } from './utils/isProd';
 import { stopServer, startServer } from './utils/serverUtils';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { imagesEndpoints } from '../api/apiEndpoints';
+import { check } from '@tauri-apps/plugin-updater';
 
+const updater = async () => {
+  const update = await check();
+  if (update) {
+    console.log(
+      `found update ${update.version} from ${update.date} with notes ${update.body}`,
+    );
+    let downloaded = 0;
+    let contentLength = 0;
+    // alternatively we could also call update.download() and update.install() separately
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          contentLength = event.data.contentLength
+            ? event.data.contentLength
+            : 0;
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case 'Progress':
+          downloaded += event.data.chunkLength;
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case 'Finished':
+          console.log('download finished');
+          break;
+      }
+    });
+
+    console.log('update installed');
+    // await relaunch();
+  }
+};
+updater();
 //Listen for window close event and stop server
 const onCloseListener = async () => {
   await getCurrentWindow().onCloseRequested(async () => {
