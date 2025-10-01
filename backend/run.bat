967a8d0691a050ec0c0ad07b4c4f158d65916528
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-10-02 04:02:25 +0530

    initial_setup

--- backend/run.bat
+++ backend/run.bat
@@ -6,6 +6,4 @@ if "%1"=="--test" (
     REM print the value of the WORKERS environment variable
     echo WORKERS: %WORKERS%
     hypercorn main:app --bind localhost:8000 --log-level debug --reload
-
-
 )
