477093e057a8422bc8eeeac1a95e3754f3be9205
DeveloperAmrit <kittyamrit005@gmail.com>
2026-01-07 22:34:14 +0530

    Merge issue solved

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,6 @@ from app.routes.albums import router as albums_router
 from app.routes.images import router as images_router
 from app.routes.face_clusters import router as face_clusters_router
 from app.routes.user_preferences import router as user_preferences_router
-<<<<<<< HEAD
-=======
-from app.routes.duplicates import router as duplicates_router
-from app.routes.celebrity import router as celebrity_router
->>>>>>> db9fab5 (Added celebrity detection feature)
 from fastapi.openapi.utils import get_openapi
 from app.logging.setup_logging import (
     configure_uvicorn_logging,
@@ -135,12 +130,6 @@ app.include_router(
 app.include_router(
     user_preferences_router, prefix="/user-preferences", tags=["User Preferences"]
 )
-<<<<<<< HEAD
-
-=======
-app.include_router(duplicates_router, prefix="/duplicates", tags=["Duplicates"])
-app.include_router(celebrity_router, prefix="/celebrity", tags=["Celebrity"])
->>>>>>> db9fab5 (Added celebrity detection feature)
 
 # Entry point for running with: python3 main.py
 if __name__ == "__main__":
